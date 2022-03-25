53597abfdc4e8ceeee7c398943ae6c9b8d72678f
duyasia <59757571+duyasia@users.noreply.github.com>
2022-03-25 19:12:47 +0700

    Update installFull.sh

--- installFull.sh
+++ installFull.sh
@@ -54,7 +54,7 @@ echo "=================================================="
 echo "\e[1m\e[32m6. Install Portainer... \e[0m" && sleep 1
 sudo docker run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data cr.portainer.io/portainer/portainer-ce:latest
 
-echo "\e[1m\e[39m Going to Portainer: https://localhost:9000 \e[0m"
+echo "\e[1m\e[39m Going to Portainer: https://localhost:9443 \e[0m"
 echo "\e[1m\e[39m Replace “localhost” with the local IP of your Raspberry Pi \e[0m"
 
 echo "=================================================="
