bf9aff4ea45256cdcd5698832e14e82048125433
duyasia <59757571+duyasia@users.noreply.github.com>
2022-05-18 00:11:34 +0700

    Update installPortainer.sh

--- installPortainer.sh
+++ installPortainer.sh
@@ -13,7 +13,7 @@ echo "    \  \::/      \  \:\/:/        \  \:\   \  \:\        \__\/ /:/       /
 echo "     \__\/        \  \::/          \__\/    \  \:\         /__/:/        \__\/      \  \:\     ";
 echo "                   \__\/                     \__\/         \__\/                     \__\/     ";
 echo "";
-echo "                             Install Portainer on Raspber Pi4 ";
+echo "                             Install Portainer on Raspber Pi 4 ";
 echo "                                     https://duy.asia ";
 echo "\e[0m"
 echo "=================================================="
