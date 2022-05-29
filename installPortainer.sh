ce824e53cd92b87836cf995192761cecad141911
duyasia <59757571+duyasia@users.noreply.github.com>
2022-05-29 21:39:33 +0700

    Update installPortainer.sh

--- installPortainer.sh
+++ installPortainer.sh
@@ -26,7 +26,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 echo "=================================================="
 
 echo "\e[1m\e[32m2. Install Portainer... \e[0m" && sleep 1
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data cr.portainer.io/portainer/portainer-ce:latest
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data cr.portainer.io/portainer/portainer-ce:latest && docker restart portainer
 
 echo "\e[1m\e[39m Going to Portainer: http://localhost:9000 or https://localhost:9443 \e[0m"
 echo "\e[1m\e[39m Replace “localhost” with the local IP of your Raspberry Pi \e[0m"
